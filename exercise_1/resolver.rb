8c3d2c400f49cb088e97de90a30b4791ccaa386b
Marshall <marshall.do.goldenowl@gmail.com>
2022-06-17 17:30:23 +0700

    first commit

--- exercise_1/resolver.rb
+++ exercise_1/resolver.rb
@@ -1,1 +1,8 @@
-# write code here...
+def check_pair_equal_sum(result, array)
+  output = []
+  sanitize_array = array.uniq
+  sanitize_array.each do |e|
+    output << [result - e, e] if sanitize_array.include?(result - e)
+  end
+  output.select { |x| x[0] < x[1] }
+end
--- exercise_1/resolver_test.rb
+++ exercise_1/resolver_test.rb
@@ -1,1 +1,14 @@
 # write code here...
+def check_pair_equal_sum(result, array)
+  output = []
+  sanitize_array = array.uniq
+  sanitize_array.each do |e|
+    output << [result - e, e] if sanitize_array.include?(result - e)
+  end
+  output.select { |x| x[0] < x[1] }
+end
+
+def test_check_pair_equal_sum
+  puts assert(check_pair_equal_sum(10, [3, 4, 5, 6, 7]) == [[4, 6], [3, 7]])
+  puts assert(check_pair_equal_sum(8, [3, 4, 5, 4, 4]) == [[3, 5]])
+end
